d6214ac0e596350b1c6aa90ed69229e2168b1dfb
chrisj909 <chrisj909@gmail.com>
2026-06-26 15:12:39 -0500

    chore: scaffold next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'product-images.tcgplayer.com' },
+      { protocol: 'https', hostname: 'i.ebayimg.com' },
+      { protocol: 'https', hostname: 'www.trollandtoad.com' },
+    ],
+  },
+}
+
+export default nextConfig
